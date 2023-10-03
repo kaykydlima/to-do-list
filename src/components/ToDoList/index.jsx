ad6276ecb53dac54ca7d3f8ab97a9ae43f899f3a
kayky <kayky140703@gmail.com>
2023-10-03 20:21:39 -0300

    feat: create to do item

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,35 +1,11 @@
-import { BsCalendarDate, BsFillPencilFill, BsFillTrashFill } from 'react-icons/bs'
-
-import { BiTimeFive } from 'react-icons/bi'
-
 import styles from './ToDoList.module.scss'
+import ToDoItem from '../ToDoItem'
 
 
 export default function ToDoList() {
   return (
     <ul className={styles.list}>
-      <li className={styles.listItem}>
-        <div className={styles.itemNameAndInput}>
-          <input type="checkbox" name="" id="checkToDo" />
-          <h3>Nome da tarefa</h3>
-        </div>
-        <div className={styles.itemDateAndTime}>
-          <div>
-            <BsCalendarDate />
-            <p>
-              11/11/1111
-            </p>
-          </div>
-          <div>
-          <BiTimeFive />
-          <p>1h</p>
-          </div>
-        </div>
-        <div className={styles.itemActions}>
-          <button className={styles.edit}><BsFillPencilFill /></button>
-          <button className={styles.delete}><BsFillTrashFill /></button>
-        </div>
-      </li>
+      <ToDoItem />
     </ul> 
   )
 }
